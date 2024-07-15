417b8c367b69698d5e635fbea4c2b6e82bc27b20
Gaurav <gauravpadwal45@gmail.com>
2024-07-15 17:07:25 +0530

    Added category tabs

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,27 +1,45 @@
 import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <section className="bg-white">
-                <div className="container flex items-center justify-between py-24">
-                    <div>
-                        <h1 className="text-6xl font-black font-sans leading-2">
-                            Super Delicious Pizza in <br />
-                            <span className="text-primary">Only 45 Minutes!</span>
-                        </h1>
-                        <p className="text-2xl mt-8 max-w-lg leading-snug">
-                            Enjoy a Free Meal if Your Order Takes More Than 45 Minutes!
-                        </p>
-                        <Button className="mt-8 text-lg rounded-full py-6 px-5 font-bold">
-                            Get your pizza now
-                        </Button>
-                    </div>
-                    <div>
-                        <Image alt="pizza-main" src={'/pizza-main.png'} width={350} height={350} />
-                    </div>
-                </div>
+          <div className="container flex items-center justify-between py-24">
+              <div>
+                  <h1 className="text-6xl font-black font-sans leading-2">
+                      Super Delicious Pizza in <br />
+                      <span className="text-primary">Only 45 Minutes!</span>
+                  </h1>
+                  <p className="text-2xl mt-8 max-w-lg leading-snug">
+                      Enjoy a Free Meal if Your Order Takes More Than 45 Minutes!
+                  </p>
+                  <Button className="mt-8 text-lg rounded-full py-6 px-5 font-bold">
+                      Get your pizza now
+                  </Button>
+              </div>
+              <div>
+                  <Image alt="pizza-main" src={'/pizza-main.png'} width={350} height={350} />
+              </div>
+          </div>
+      </section>
+
+      <section>
+          <div className="container py-12">
+              <Tabs defaultValue="pizza" className="w-[400px]">
+                  <TabsList>
+                      <TabsTrigger value="pizza" className="text-md">
+                          Pizza
+                      </TabsTrigger>
+                      <TabsTrigger value="beverages" className="text-md">
+                          Beverages
+                      </TabsTrigger>
+                  </TabsList>
+                  <TabsContent value="pizza">Pizza list</TabsContent>
+                  <TabsContent value="beverages">Beverages list </TabsContent>
+              </Tabs>
+          </div>
       </section>
     </>
   );
