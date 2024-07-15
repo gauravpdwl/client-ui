978633a3109841399e04a30c4b8b8c7ab1c2c803
Gaurav <gauravpadwal45@gmail.com>
2024-07-15 10:11:55 +0530

    Added logo and selectbox

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="">
+    <>
       <h1>Welcome to Pizza Shop</h1>
       <Button variant="link">Button</Button>
-    </main>
+    </>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Manrope } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
+import Header from '@/components/custom/header';
 
 const manrope = Manrope({ subsets: ['latin'], variable: '--font-manrope' });
 
@@ -21,8 +22,12 @@ export default function RootLayout({
                 className={cn(
                     'min-h-screen bg-background font-manrope antialiased',
                     manrope.variable
-                )}>
-                {children}
+                )}
+            >
+                <Header/>
+                <main>
+                    {children}
+                </main>
             </body>
         </html>
     );
