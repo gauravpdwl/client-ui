8d9b6ad23784f2b9825c18b1f546d7d70b6ed593
Gaurav <gauravpadwal45@gmail.com>
2024-07-18 02:51:51 +0530

    fetch categories

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import ProductCard, { Product } from './components/productCard';
+import { Category } from '@/lib/types';
 
 const products: Product[] = [
     {
@@ -41,7 +42,22 @@ const products: Product[] = [
     },
 ];
 
-export default function Home() {
+export default async function Home() {
+
+    const categoryResponse = await fetch(`${process.env.BACKEND_URL}/api/catalog/categories`, {
+        next: {
+            revalidate: 3600, // 1 hour
+        },
+    });
+
+    if (!categoryResponse.ok) {
+        throw new Error('Failed to fetch categories');
+    }
+
+    const categories: Category[] = await categoryResponse.json();
+
+    // console.log(categories);
+
     return (
         <>
             <section className="bg-white">
@@ -68,12 +84,20 @@ export default function Home() {
                 <div className="container py-12">
                     <Tabs defaultValue="pizza">
                         <TabsList>
-                            <TabsTrigger value="pizza" className="text-md">
-                                Pizza
-                            </TabsTrigger>
-                            <TabsTrigger value="beverages" className="text-md">
-                                Beverages
-                            </TabsTrigger>
+                            {categories.map((category) => {
+                                    return (
+                                        <TabsTrigger
+                                            key={category._id}
+                                            value={category._id}
+                                            className="text-md">
+                                            {category.name}
+                                        </TabsTrigger>
+                                    );
+                                })
+                            }
+                                {/* <TabsTrigger value="beverages" className="text-md">
+                                    Beverages
+                                </TabsTrigger> */}
                         </TabsList>
                         <TabsContent value="pizza">
                             <div className="grid grid-cols-4 gap-6 mt-6">
