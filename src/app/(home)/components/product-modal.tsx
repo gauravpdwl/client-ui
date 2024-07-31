5649c3403d1eff7e08cc63218d57ec59d8fe19ba
Gaurav <gauravpadwal45@gmail.com>
2024-07-31 18:02:55 +0530

    Moved toping selection function to product-modal component from topping-list component

--- src/app/(home)/components/product-modal.tsx
+++ src/app/(home)/components/product-modal.tsx
@@ -7,7 +7,7 @@ import { ShoppingCart } from 'lucide-react';
 import React, { startTransition, Suspense, useState } from 'react';
 import ToppingList from './topping-list';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
-import { Product } from '@/lib/types';
+import { Product, Topping } from '@/lib/types';
 import { Label } from '@/components/ui/label';
 
 type ChosenConfig = {
@@ -17,6 +17,23 @@ type ChosenConfig = {
 const ProductModal = ({ product }: { product: Product }) => {
 
     const [chosenConfig, setChosenConfig] = useState<ChosenConfig>();
+
+    const [selectedToppings, setSelectedToppings] = React.useState<Topping[]>([]);
+
+    const handleCheckBoxCheck = (topping: Topping) => {
+        const isAlreadyExists = selectedToppings.some(
+            (element: Topping) => element.id === topping.id
+        );
+
+        startTransition(() => {
+            if (isAlreadyExists) {
+                setSelectedToppings((prev) => prev.filter((elm: Topping) => elm.id !== topping.id));
+                return;
+            }
+
+            setSelectedToppings((prev: Topping[]) => [...prev, topping]);
+        });
+    };
 
     const handleAddToCart = () => {
         // todo: add to cart logic
@@ -92,7 +109,10 @@ const ProductModal = ({ product }: { product: Product }) => {
                         })}
 
                         <Suspense fallback={'Toppings loading...'}>
-                            <ToppingList />
+                            <ToppingList 
+                                selectedToppings={selectedToppings}
+                                handleCheckBoxCheck={handleCheckBoxCheck}
+                            />
                         </Suspense>
 
                         <div className="flex items-center justify-between mt-12">
--- src/app/(home)/components/topping-list.tsx
+++ src/app/(home)/components/topping-list.tsx
@@ -2,7 +2,10 @@ import React, { startTransition, useEffect, useState } from 'react';
 import ToppingCard from './topping-card';
 import { Topping } from '@/lib/types';
 
-const ToppingList = () => {
+const ToppingList = ({selectedToppings, handleCheckBoxCheck} : {
+    selectedToppings: Topping[],
+    handleCheckBoxCheck: (topping: Topping) => void
+}) => {
     const [toppings, setToppings] = useState<Topping[]>([]);
     useEffect(() => {
         const fetchData = async () => {
@@ -17,22 +20,7 @@ const ToppingList = () => {
         fetchData();
     }, []);
 
-    const [selectedToppings, setSelectedToppings] = React.useState<Topping[]>([]);
-
-    const handleCheckBoxCheck = (topping: Topping) => {
-        const isAlreadyExists = selectedToppings.some(
-            (element: Topping) => element.id === topping.id
-        );
-
-        startTransition(() => {
-            if (isAlreadyExists) {
-                setSelectedToppings((prev) => prev.filter((elm: Topping) => elm.id !== topping.id));
-                return;
-            }
-
-            setSelectedToppings((prev: Topping[]) => [...prev, topping]);
-        });
-    };
+    
 
     return (
         <section className="mt-6">
