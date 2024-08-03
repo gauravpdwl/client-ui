b06723d3592120b26506cdc68573627eff3416ad
Gaurav <gauravpadwal45@gmail.com>
2024-08-03 07:34:17 +0530

    Close model after product is added to cart

--- src/app/(home)/components/product-modal.tsx
+++ src/app/(home)/components/product-modal.tsx
@@ -19,6 +19,8 @@ type ChosenConfig = {
 
 const ProductModal = ({ product }: { product: Product }) => {
 
+    const [dialogOpen, setDialogOpen] = useState(false);
+
     const cartItems = useAppSelector((state) => state.cart.cartItems);
 
     const dispatch = useAppDispatch();
@@ -93,6 +95,7 @@ const ProductModal = ({ product }: { product: Product }) => {
             qty: 1,
         };
         dispatch(addToCart(itemToAdd));
+        setDialogOpen(false);
     };
 
     const handleRadioChange = (key: string, data: string) => {
@@ -111,7 +114,7 @@ const ProductModal = ({ product }: { product: Product }) => {
     };
 
     return (
-        <Dialog>
+        <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
             <DialogTrigger className="bg-orange-200 hover:bg-orange-300 text-orange-500 px-6 py-2 rounded-full shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150">
                 Choose
             </DialogTrigger>
