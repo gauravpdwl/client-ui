593911e0ed82d2d12128a86859244aa62f2d0467
Gaurav <gauravpadwal45@gmail.com>
2024-08-13 14:52:47 +0530

    added session functionality

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -7,10 +7,13 @@ import { Tenant } from '@/lib/types';
 // import CartCounter from './cart-counter';
 import dynamic from 'next/dynamic';
 import TenantSelect from './tenant-select';
+import { getSession } from '@/lib/session';
 
 const CartCounterWithoutSSR = dynamic(() => import('./cart-counter'), { ssr: false });
 
 const Header = async () => {
+
+    const session = await getSession();
 
     const tenantsResponse = await fetch(`${process.env.BACKEND_URL}/api/auth/tenants/all?perPage=100`, {
         next: {
@@ -64,7 +67,7 @@ const Header = async () => {
                         <Phone />
                         <span>+91 9800 098 998</span>
                     </div>
-                    <Button size={'sm'}>Logout</Button>
+                    <Button size={'sm'}>{session ? 'Logout' : 'Login'}</Button>
                 </div>
             </nav>
         </header>
