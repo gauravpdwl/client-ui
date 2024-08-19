93841bf2df6792f13c5bdfd6dd8ce7d21cd14657
gauravpdwl <gauravpadwal45@gmail.com>
2024-08-19 17:21:56 +0530

    add discount with coupon code

--- src/lib/http/api.ts
+++ src/lib/http/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { CouponCodeData } from '../types';
 
 export const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
@@ -17,3 +18,6 @@ export const addAddress = (customerId: string | undefined, address: string) =>
     api.patch(`${ORDER_SERVICE_PREFIX}/customer/addresses/${customerId}`, {
         address,
     });
+
+export const verifyCoupon = (data: CouponCodeData) =>
+    api.post(`${ORDER_SERVICE_PREFIX}/coupons/verify`, data);
