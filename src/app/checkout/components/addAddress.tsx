36d1ab667e97ed4e1f874a69766c22e9966b42a6
gauravpdwl <gauravpadwal45@gmail.com>
2024-08-19 12:15:24 +0530

    fix form event propagation

--- src/app/checkout/components/addAddress.tsx
+++ src/app/checkout/components/addAddress.tsx
@@ -47,10 +47,12 @@ const AddAdress = ({ customerId }: { customerId: string | undefined }) => {
         },
     });
 
-    const handleAddressAdd = (data: z.infer<typeof formSchema>) => {
-        console.log('data', data);
+    const handleAddressAdd = (e: React.FormEvent<HTMLFormElement>) => {
+        e.stopPropagation();
 
-        mutate(data.address);
+        return addressForm.handleSubmit((data: z.infer<typeof formSchema>) => {
+            mutate(data.address);
+        })(e);
     };
 
     // todo: Display error if any (useMutation -> isError)
@@ -64,7 +66,7 @@ const AddAdress = ({ customerId }: { customerId: string | undefined }) => {
             </DialogTrigger>
             <DialogContent className="sm:max-w-[425px]">
                 <Form {...addressForm}>
-                    <form onSubmit={addressForm.handleSubmit(handleAddressAdd)}>
+                    <form onSubmit={handleAddressAdd}>
                         <DialogHeader>
                             <DialogTitle>Add Address</DialogTitle>
                             <DialogDescription>
