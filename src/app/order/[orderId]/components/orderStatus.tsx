59f4e2a2be45ccbebd9491de3be6872f516ae829
gauravpdwl <gauravpadwal45@gmail.com>
2024-09-04 12:55:06 +0530

    change stepper programatically

--- src/app/order/[orderId]/components/orderStatus.tsx
+++ src/app/order/[orderId]/components/orderStatus.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Step, StepItem, Stepper } from '@/components/stepper';
+import { Step, StepItem, Stepper, useStepper } from '@/components/stepper';
 import { CheckCheck, FileCheck, Microwave, Package, PackageCheck } from 'lucide-react';
 import React from 'react';
 
@@ -12,13 +12,25 @@ const steps = [
     { label: 'Delivered', icon: CheckCheck, description: 'Order completed' },
 ] satisfies StepItem[];
 
+const StepperChanger = () => {
+    const { setStep, nextStep } = useStepper();
+
+    React.useEffect(() => {
+        setInterval(() => {
+            nextStep();
+        }, 2000);
+    }, []);
+    return <></>;
+};
+
 const OrderStatus = () => {
     return (
-        <Stepper initialStep={3} steps={steps} variant="circle-alt" className="py-8">
+        <Stepper initialStep={1} steps={steps} variant="circle-alt" className="py-8">
             {steps.map(({ label, icon }) => {
                 // eslint-disable-next-line react/jsx-key
-                return <Step label={label} icon={icon} checkIcon={icon}></Step>;
+                return <Step key={label} label={label} icon={icon} checkIcon={icon}></Step>;
             })}
+            <StepperChanger />
         </Stepper>
     );
 };
