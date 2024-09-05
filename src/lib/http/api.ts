b1e59d882047d10b32c72820ea60b991a8aeeb48
gauravpdwl <gauravpadwal45@gmail.com>
2024-09-06 04:37:24 +0530

    render order status dynamically

--- src/lib/http/api.ts
+++ src/lib/http/api.ts
@@ -28,3 +28,6 @@ export const createOrder = (data: OrderData, idempotencyKey: string) =>
             'Idempotency-Key': idempotencyKey,
         },
     });
+
+export const getSingleOrder = (orderId: string) =>
+    api.get(`${ORDER_SERVICE_PREFIX}/orders/${orderId}?fields=orderStatus`);
